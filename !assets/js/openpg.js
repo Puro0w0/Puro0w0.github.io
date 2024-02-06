3d6eee6b68b33bd04f17e8f8adec1f5ed4d565e6
Puro <94943721+Puro0w0@users.noreply.github.com>
2024-02-06 17:50:29 -0600

    Update openpg.js

--- !assets/js/openpg.js
+++ !assets/js/openpg.js
@@ -1,6 +1,6 @@
 const userAgent = navigator.userAgent;
 
-if (navigator.userAgent.includes("Firefox")) {
+if (navigator.userAgent.indexOf("Firefox")) {
     alert("Sorry, this page is not supported in Firefox.");
     window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUXbmV2ZXIgZ29ubmEgZ2l2ZSB5b3UgdXA%3D";
 }
