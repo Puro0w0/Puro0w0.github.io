c8c43d2f6892f09a38bae050cb83ba055574779d
Puro <94943721+Puro0w0@users.noreply.github.com>
2024-02-06 17:38:38 -0600

    Update openpg.js

--- !assets/js/openpg.js
+++ !assets/js/openpg.js
@@ -1,7 +1,7 @@
 if (navigator.userAgent.includes("Firefox")) {
     alert("Sorry, this page is not supported in Firefox.");
     window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUXbmV2ZXIgZ29ubmEgZ2l2ZSB5b3UgdXA%3D";
-
+}
 function cookie_clicker() {
     window.location.href = '/cookie-clicker/en';
 }
