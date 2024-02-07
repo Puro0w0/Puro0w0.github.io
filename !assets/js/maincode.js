119442104922124db6d97fb16055909ec7f8ee87
Puro <94943721+Puro0w0@users.noreply.github.com>
2024-02-06 18:29:17 -0600

    Update maincode.js

--- !assets/js/maincode.js
+++ !assets/js/maincode.js
@@ -1,13 +1,5 @@
-const userAgent = navigator.userAgent;
-
 var nice = "Nice bro";
 
-if (navigator.userAgent.includes("Firefox")) {
-    alert("Sorry, Firefox aren't allowed on this site, have fun being rick rolled!");
-    window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUXbmV2ZXIgZ29ubmEgZ2l2ZSB5b3UgdXA%3D";
-} else if (userAgent.includes('Chrome')) {
-    console.log(nice)
-}
 function cookie_clicker() {
     window.location.href = '/cookie-clicker/en';
 }
