3ca655cc8d505a9c5fd077fcbaf288ded28d3f76
Puro <94943721+Puro0w0@users.noreply.github.com>
2024-02-07 12:47:40 -0600

    Update maincode.js

--- !assets/js/maincode.js
+++ !assets/js/maincode.js
@@ -7,5 +7,5 @@ function about_me() {
     window.location.href = '/about-me/fr';
 }
 function about_furries() {
-    window.location.href = 'https://fr.wikipedia.org/wiki/Fandom_furry';
+    window.location.href = "https://fr.wikipedia.org/wiki/Fandom_furry";
 }
