7586038eb791be1da481cc485326616447bee60c
Puro <94943721+Puro0w0@users.noreply.github.com>
2024-03-02 09:57:54 -0600

    Update maincode.js

--- !assets/js/maincode.js
+++ !assets/js/maincode.js
@@ -3,6 +3,9 @@ var nice = "Nice bro";
 function cookie_clicker() {
     window.location.href = '/cookie-clicker/en';
 }
+function puro_engine() {
+    window.location.href = '/puros-engine   
+}
 function about_me() {
     window.location.href = '/about-me/fr';
 }
